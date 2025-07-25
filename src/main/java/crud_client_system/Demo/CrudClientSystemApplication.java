dd62096125289e8e9db9734318247fa55338e85c
Natanael Rodrigues <natanaelrodriguessouza13@gmail.com>
2025-07-24 22:01:28 -0300

    first commit

---
+++ src/main/java/crud_client_system/Demo/CrudClientSystemApplication.java
@@ -0,0 +1,13 @@
+package crud_client_system.Demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudClientSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudClientSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/crud_client_system/Demo/CrudClientSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package crud_client_system.Demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudClientSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
