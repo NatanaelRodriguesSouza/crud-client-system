a61496ceb7ea8972500e9f275b468e2ed1df2880
Natanael Rodrigues <natanaelrodriguessouza13@gmail.com>
2025-07-25 01:00:18 -0300

    Deletar recurso

--- src/main/java/crud_client_system/Demo/Controllers/ClientController.java
+++ src/main/java/crud_client_system/Demo/Controllers/ClientController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -43,10 +44,14 @@ public class ClientController {
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
 		return ResponseEntity.created(uri).body(dto);
 	}
-	
 	@PutMapping(value = "/{id}")
 	public ResponseEntity<ClientDTO> update(@PathVariable long id,@RequestBody ClientDTO dto){
 		dto = service.update(id, dto);
 		return ResponseEntity.ok(dto);
+	}
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable long id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
 	}
 }
--- src/main/java/crud_client_system/Demo/Services/ClientService.java
+++ src/main/java/crud_client_system/Demo/Services/ClientService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import crud_client_system.Demo.DTO.ClientDTO;
@@ -49,5 +50,9 @@ public class ClientService {
 		client.setIncome(dto.getIncome());
 		client.setName(dto.getName());
 		client = repository.save(client);
+	}
+	@Transactional(propagation = Propagation.SUPPORTS)
+	public void delete(Long id) {
+		repository.deleteById(id);
 	}
 }
